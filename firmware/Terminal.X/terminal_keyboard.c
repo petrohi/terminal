e7105eb7ca0c749012b5edb148d439868b9834aa
Peter Hizalev <peter.hizalev@gmail.com>
2020-08-04 21:32:56 -0700

    Shift-Alt-Ctrl modifiers

--- firmware/Terminal.X/terminal_keyboard.c
+++ firmware/Terminal.X/terminal_keyboard.c
@@ -1,9 +1,13 @@
 #include "terminal_internal.h"
 
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
 #include "terminal_keyboard.h"
 
 #define FIRST_REPEAT_COUNTER 500
 #define NEXT_REPEAT_COUNTER 33
+#define ESCAPE_KEY_BUFFER_SIZE 16
 
 static size_t get_case(struct terminal *terminal) {
   return terminal->lock_state.caps ^ terminal->shift_state;
@@ -217,72 +221,121 @@ static const struct keys_entry *default_entries = (struct keys_entry[]){
         KEY_ROUTER(get_ctrl, KEY_ROUTER(get_shift, KEY_CHR('/'), KEY_CHR('?')),
                    KEY_CHR('\x1f')),
     [KEY_CAPS_LOCK] = KEY_HANDLER(handle_caps_lock),
-    [KEY_F1] = KEY_STR("\033OP"),
-    [KEY_F2] = KEY_STR("\033OQ"),
-    [KEY_F3] = KEY_STR("\033OR"),
-    [KEY_F4] = KEY_STR("\033OS"),
-    [KEY_F5] = KEY_STR("\033[15~"),
-    [KEY_F6] = KEY_STR("\033[17~"),
-    [KEY_F7] = KEY_STR("\033[18~"),
-    [KEY_F8] = KEY_STR("\033[19~"),
-    [KEY_F9] = KEY_STR("\033[20~"),
-    [KEY_F10] = KEY_STR("\033[21~"),
-    [KEY_F11] = KEY_STR("\033[23~"),
-    [KEY_F12] = KEY_STR("\033[24~"),
+    [KEY_F1] = KEY_SS3_MOD_CSI("P"),
+    [KEY_F2] = KEY_SS3_MOD_CSI("Q"),
+    [KEY_F3] = KEY_SS3_MOD_CSI("R"),
+    [KEY_F4] = KEY_SS3_MOD_CSI("S"),
+    [KEY_F5] = KEY_CSI("15~"),
+    [KEY_F6] = KEY_CSI("17~"),
+    [KEY_F7] = KEY_CSI("18~"),
+    [KEY_F8] = KEY_CSI("19~"),
+    [KEY_F9] = KEY_CSI("20~"),
+    [KEY_F10] = KEY_CSI("21~"),
+    [KEY_F11] = KEY_CSI("23~"),
+    [KEY_F12] = KEY_CSI("24~"),
     [KEY_SCROLL_LOCK] = KEY_HANDLER(handle_scroll_lock),
-    [KEY_INSERT] = KEY_STR("\033[2~"),
-    [KEY_HOME] =
-        KEY_ROUTER(get_cursor_key_mode, KEY_STR("\033[H"), KEY_STR("\033OH")),
-    [KEY_PAGEUP] = KEY_STR("\033[5~"),
-    [KEY_DELETE] = KEY_ROUTER(get_ctrl_alt, KEY_STR("\033[3~"),
+    [KEY_INSERT] = KEY_CSI("2~"),
+    [KEY_HOME] = KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1_1("H"), KEY_SS3("H")),
+    [KEY_PAGEUP] = KEY_CSI("5~"),
+    [KEY_DELETE] = KEY_ROUTER(get_ctrl_alt, KEY_CSI("3~"),
                               KEY_HANDLER(handle_ctrl_alt_delete)),
-    [KEY_END1] =
-        KEY_ROUTER(get_cursor_key_mode, KEY_STR("\033[F"), KEY_STR("\033OF")),
-    [KEY_PAGEDOWN] = KEY_STR("\033[6~"),
+    [KEY_END1] = KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("F"), KEY_SS3("F")),
+    [KEY_PAGEDOWN] = KEY_CSI("6~"),
     [KEY_RIGHTARROW] = KEY_ROUTER(
-        get_ansi_mode, KEY_STR("\033C"),
-        KEY_ROUTER(get_cursor_key_mode, KEY_STR("\033[C"), KEY_STR("\033OC"))),
+        get_ansi_mode, KEY_ESC("C"),
+        KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("C"), KEY_SS3("C"))),
     [KEY_LEFTARROW] = KEY_ROUTER(
-        get_ansi_mode, KEY_STR("\033D"),
-        KEY_ROUTER(get_cursor_key_mode, KEY_STR("\033[D"), KEY_STR("\033OD"))),
+        get_ansi_mode, KEY_ESC("D"),
+        KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("D"), KEY_SS3("D"))),
     [KEY_DOWNARROW] = KEY_ROUTER(
-        get_ansi_mode, KEY_STR("\033B"),
-        KEY_ROUTER(get_cursor_key_mode, KEY_STR("\033[B"), KEY_STR("\033OB"))),
+        get_ansi_mode, KEY_ESC("B"),
+        KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("B"), KEY_SS3("B"))),
     [KEY_UPARROW] = KEY_ROUTER(
-        get_ansi_mode, KEY_STR("\033A"),
-        KEY_ROUTER(get_cursor_key_mode, KEY_STR("\033[A"), KEY_STR("\033OA"))),
+        get_ansi_mode, KEY_ESC("A"),
+        KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("A"), KEY_SS3("A"))),
     [KEY_KEYPAD_NUM_LOCK_AND_CLEAR] = KEY_HANDLER(handle_num_lock),
-    [KEY_KEYPAD_SLASH] = KEY_ROUTER(get_num, KEY_STR("\033Oo"), KEY_CHR('/')),
-    [KEY_KEYPAD_ASTERIKS] =
-        KEY_ROUTER(get_num, KEY_STR("\033Oj"), KEY_CHR('*')),
-    [KEY_KEYPAD_MINUS] = KEY_ROUTER(get_num, KEY_STR("\033Om"), KEY_CHR('-')),
-    [KEY_KEYPAD_PLUS] = KEY_ROUTER(get_num, KEY_STR("\033Ok"), KEY_CHR('+')),
+    [KEY_KEYPAD_SLASH] = KEY_ROUTER(get_num, KEY_SS3("o"), KEY_CHR('/')),
+    [KEY_KEYPAD_ASTERIKS] = KEY_ROUTER(get_num, KEY_SS3("j"), KEY_CHR('*')),
+    [KEY_KEYPAD_MINUS] = KEY_ROUTER(get_num, KEY_SS3("m"), KEY_CHR('-')),
+    [KEY_KEYPAD_PLUS] = KEY_ROUTER(get_num, KEY_SS3("k"), KEY_CHR('+')),
     [KEY_KEYPAD_ENTER] = KEY_ROUTER(
-        get_num, KEY_STR("\033OM"),
+        get_num, KEY_SS3("M"),
         KEY_ROUTER(get_new_line_mode, KEY_CHR('\x0d'), KEY_STR("\x0d\x0a"))),
-    [KEY_KEYPAD_1_END] = KEY_ROUTER(get_num, KEY_STR("\033OF"), KEY_CHR('1')),
+    [KEY_KEYPAD_1_END] = KEY_ROUTER(get_num, KEY_SS3("F"), KEY_CHR('1')),
     [KEY_KEYPAD_2_DOWN_ARROW] =
-        KEY_ROUTER(get_num, KEY_STR("\033[B"), KEY_CHR('2')),
-    [KEY_KEYPAD_3_PAGEDN] =
-        KEY_ROUTER(get_num, KEY_STR("\033[6~"), KEY_CHR('3')),
+        KEY_ROUTER(get_num, KEY_CSI_MOD_1("B"), KEY_CHR('2')),
+    [KEY_KEYPAD_3_PAGEDN] = KEY_ROUTER(get_num, KEY_CSI("6~"), KEY_CHR('3')),
     [KEY_KEYPAD_4_LEFT_ARROW] =
-        KEY_ROUTER(get_num, KEY_STR("\033[D"), KEY_CHR('4')),
-    [KEY_KEYPAD_5] = KEY_ROUTER(get_num, KEY_STR("\033[E"), KEY_CHR('5')),
+        KEY_ROUTER(get_num, KEY_CSI_MOD_1("D"), KEY_CHR('4')),
+    [KEY_KEYPAD_5] = KEY_ROUTER(get_num, KEY_CSI_MOD_1("E"), KEY_CHR('5')),
     [KEY_KEYPAD_6_RIGHT_ARROW] =
-        KEY_ROUTER(get_num, KEY_STR("\033[C"), KEY_CHR('6')),
-    [KEY_KEYPAD_7_HOME] = KEY_ROUTER(get_num, KEY_STR("\033OH"), KEY_CHR('7')),
-    [KEY_KEYPAD_8_UP_ARROW] =
-        KEY_ROUTER(get_num, KEY_STR("\033[A"), KEY_CHR('8')),
-    [KEY_KEYPAD_9_PAGEUP] =
-        KEY_ROUTER(get_num, KEY_STR("\033[5~"), KEY_CHR('9')),
-    [KEY_KEYPAD_0_INSERT] =
-        KEY_ROUTER(get_num, KEY_STR("\033[2~"), KEY_CHR('0')),
+        KEY_ROUTER(get_num, KEY_CSI_MOD_1("C"), KEY_CHR('6')),
+    [KEY_KEYPAD_7_HOME] = KEY_ROUTER(get_num, KEY_SS3("H"), KEY_CHR('7')),
+    [KEY_KEYPAD_8_UP_ARROW] = KEY_ROUTER(get_num, KEY_CSI_MOD_1("A"), KEY_CHR('8')),
+    [KEY_KEYPAD_9_PAGEUP] = KEY_ROUTER(get_num, KEY_CSI("5~"), KEY_CHR('9')),
+    [KEY_KEYPAD_0_INSERT] = KEY_ROUTER(get_num, KEY_CSI("2~"), KEY_CHR('0')),
     [KEY_KEYPAD_DECIMAL_SEPARATOR_DELETE] =
         KEY_ROUTER(get_num,
-                   KEY_ROUTER(get_ctrl_alt, KEY_STR("\033[3~"),
+                   KEY_ROUTER(get_ctrl_alt, KEY_CSI("3~"),
                               KEY_HANDLER(handle_ctrl_alt_delete)),
                    KEY_CHR('.')),
 };
+
+static void transmit_escape_key(struct terminal *terminal, character_t control,
+                                const char *string, size_t mod_prepend_1s,
+                                bool mod_force_csi) {
+  size_t l = strlen(string);
+  char buffer[ESCAPE_KEY_BUFFER_SIZE];
+  memset(buffer, 0, ESCAPE_KEY_BUFFER_SIZE);
+  size_t i = 0;
+
+  buffer[i++] = '\033';
+
+  if (control) {
+    if (mod_force_csi &&
+        (terminal->shift_state || terminal->alt_state || terminal->ctrl_state))
+      buffer[i++] = '[';
+    else
+      buffer[i++] = control;
+  }
+
+  while (l--) {
+    if (control && !l) {
+      uint8_t modifier = terminal->shift_state | (terminal->alt_state << 1) |
+                         (terminal->ctrl_state << 2);
+      if (modifier) {
+
+        if (isdigit((int) buffer[i - 1]))
+          buffer[i++] = ';';
+
+        while (mod_prepend_1s--) {
+          buffer[i++] = '1';
+          buffer[i++] = ';';
+        }
+
+        i += sprintf(buffer + i, "%d", modifier + 1);
+      }
+    }
+
+    buffer[i++] = *(string++);
+  }
+
+  terminal_uart_transmit_string(terminal, buffer);
+}
+
+static void transmit_character_key(struct terminal *terminal,
+                                   character_t character) {
+  if (terminal->alt_state)
+    terminal_uart_transmit_character(terminal, '\033');
+
+  terminal_uart_transmit_character(terminal, character);
+}
+
+static void transmit_string_key(struct terminal *terminal, const char *string) {
+  character_t character = 0;
+  while ((character = *(string++)))
+   transmit_character_key(terminal, character);
+}
 
 static void handle_key(struct terminal *terminal,
                        const struct keys_entry *key) {
@@ -292,10 +345,27 @@ static void handle_key(struct terminal *terminal,
   case IGNORE:
     break;
   case CHR:
-    terminal_uart_transmit_character(terminal, key->data.character);
+    transmit_character_key(terminal, key->data.character);
     break;
   case STR:
-    terminal_uart_transmit_string(terminal, key->data.string);
+    transmit_string_key(terminal, key->data.string);
+    break;
+  case ESC:
+    transmit_escape_key(terminal, 0, key->data.string, 0, false);
+    break;
+  case CSI:
+  case CSI_MOD_1:
+  case CSI_MOD_1_1:
+    transmit_escape_key(
+        terminal, '[', key->data.string,
+        (key->type == CSI_MOD_1_1 ? 2 : (key->type == CSI_MOD_1 ? 1 : 0)),
+        false);
+    break;
+  case SS3:
+  case SS3_MOD_CSI:
+    transmit_escape_key(terminal, 'O', key->data.string,
+                        key->type == SS3_MOD_CSI ? 1 : 0,
+                        key->type == SS3_MOD_CSI);
     break;
   case HANDLER:
     key->data.handler(terminal);
--- firmware/Terminal.X/terminal_keyboard.h
+++ firmware/Terminal.X/terminal_keyboard.h
@@ -6,6 +6,12 @@ enum keys_entry_type {
   IGNORE,
   CHR,
   STR,
+  ESC,
+  CSI,
+  CSI_MOD_1,
+  CSI_MOD_1_1,
+  SS3,
+  SS3_MOD_CSI,
   HANDLER,
   ROUTER,
 };
@@ -37,6 +43,30 @@ struct keys_entry {
   {                                                                            \
     STR, { .string = s }                                                       \
   }
+#define KEY_ESC(s)                                                             \
+  {                                                                            \
+    ESC, { .string = s }                                                       \
+  }
+#define KEY_CSI(s)                                                             \
+  {                                                                            \
+    CSI, { .string = s }                                                       \
+  }
+#define KEY_CSI_MOD_1(s)                                                       \
+  {                                                                            \
+    CSI_MOD_1, { .string = s }                                                 \
+  }
+#define KEY_CSI_MOD_1_1(s)                                                     \
+  {                                                                            \
+    CSI_MOD_1_1, { .string = s }                                               \
+  }
+#define KEY_SS3(s)                                                             \
+  {                                                                            \
+    SS3, { .string = s }                                                       \
+  }
+#define KEY_SS3_MOD_CSI(s)                                                     \
+  {                                                                            \
+    SS3_MOD_CSI, { .string = s }                                               \
+  }
 #define KEY_HANDLER(h)                                                         \
   {                                                                            \
     HANDLER, { .handler = h }                                                  \
@@ -44,8 +74,8 @@ struct keys_entry {
 #define KEY_ROUTER(r, ...)                                                     \
   {                                                                            \
     ROUTER, {                                                                  \
-      .router = &(const struct keys_router) {                         \
-        r, (const struct keys_entry[]) { __VA_ARGS__ }                \
+      .router = &(const struct keys_router) {                                  \
+        r, (const struct keys_entry[]) { __VA_ARGS__ }                         \
       }                                                                        \
     }                                                                          \
   }
