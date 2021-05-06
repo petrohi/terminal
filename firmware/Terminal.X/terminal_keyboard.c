8acc05889b4b05511d7c6b8c75f75e6d7359645b
Stefan Karlsson <sbkarlsson@gmail.com>
2021-05-06 14:11:12 +0200

    Add Swedish keyboard layout

--- firmware/Terminal.X/terminal_keyboard.c
+++ firmware/Terminal.X/terminal_keyboard.c
@@ -963,6 +963,349 @@ static const struct keys_entry de_entries[] = {
         KEY_CHR(',')),
 };
 
+static const struct keys_entry se_entries[] = {
+    [KEY_A] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('a'), KEY_CHR('A')),
+                   KEY_CHR('\x01')),
+    [KEY_B] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('b'), KEY_CHR('B')),
+                   KEY_CHR('\x02')),
+    [KEY_C] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('c'), KEY_CHR('C')),
+                   KEY_CHR('\x03')),
+    [KEY_D] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('d'), KEY_CHR('D')),
+                   KEY_CHR('\x04')),
+    [KEY_E] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_case, KEY_CHR('e'), KEY_CHR('E')),
+                              KEY_CHR('\x05')),
+                   KEY_STR("€")),
+    [KEY_F] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('f'), KEY_CHR('F')),
+                   KEY_CHR('\x06')),
+    [KEY_G] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('g'), KEY_CHR('G')),
+                   KEY_CHR('\x07')),
+    [KEY_H] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('h'), KEY_CHR('H')),
+                   KEY_CHR('\x08')),
+    [KEY_I] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('i'), KEY_CHR('I')),
+                   KEY_CHR('\x09')),
+    [KEY_J] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('j'), KEY_CHR('J')),
+                   KEY_CHR('\x0A')),
+    [KEY_K] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('k'), KEY_CHR('K')),
+                   KEY_CHR('\x0B')),
+    [KEY_L] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('l'), KEY_CHR('L')),
+                   KEY_CHR('\x0C')),
+    [KEY_M] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_case, KEY_CHR('m'), KEY_CHR('M')),
+                              KEY_CHR('\x0D')),
+                   KEY_STR("µ")),
+    [KEY_N] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('n'), KEY_CHR('N')),
+                   KEY_CHR('\x0E')),
+    [KEY_O] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('o'), KEY_CHR('O')),
+                   KEY_CHR('\x0F')),
+    [KEY_P] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('p'), KEY_CHR('P')),
+                   KEY_CHR('\x10')),
+    [KEY_Q] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('q'), KEY_CHR('Q')),
+                   KEY_HANDLER(handle_ctrl_q)),
+    [KEY_R] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('r'), KEY_CHR('R')),
+                   KEY_CHR('\x12')),
+    [KEY_S] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('s'), KEY_CHR('S')),
+                   KEY_HANDLER(handle_ctrl_s)),
+    [KEY_T] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('t'), KEY_CHR('T')),
+                   KEY_CHR('\x14')),
+    [KEY_U] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('u'), KEY_CHR('U')),
+                   KEY_CHR('\x15')),
+    [KEY_V] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('v'), KEY_CHR('V')),
+                   KEY_CHR('\x16')),
+    [KEY_W] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('w'), KEY_CHR('W')),
+                   KEY_CHR('\x17')),
+    [KEY_X] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('x'), KEY_CHR('X')),
+                   KEY_CHR('\x18')),
+    [KEY_Y] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('y'), KEY_CHR('Y')),
+                   KEY_CHR('\x19')),
+    [KEY_Z] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_case, KEY_CHR('z'), KEY_CHR('Z')),
+                   KEY_CHR('\x1A')),
+    [KEY_1_EXCLAMATION_MARK] =
+        KEY_ROUTER(get_shift, KEY_CHR('1'), KEY_CHR('!')),
+    [KEY_2_AT] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_shift, KEY_CHR('2'), KEY_CHR('"')),
+                              KEY_CHR('\0')),
+                   KEY_CHR('@')),
+    [KEY_3_NUMBER_SIGN] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_shift, KEY_CHR('3'), KEY_CHR('#')),
+                              KEY_CHR('\x1B')),
+                   KEY_STR("£")),
+    [KEY_4_DOLLAR] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_shift, KEY_CHR('4'), KEY_STR("¤")),
+                              KEY_CHR('\x1C')),
+                   KEY_CHR('$')),
+    [KEY_5_PERCENT] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_shift, KEY_CHR('5'), KEY_CHR('%')),
+                   KEY_CHR('\x1D')),
+    [KEY_6_CARET] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_shift, KEY_CHR('6'), KEY_CHR('&')),
+                   KEY_CHR('\x1E')),
+    [KEY_7_AMPERSAND] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_shift, KEY_CHR('7'), KEY_CHR('/')),
+                              KEY_CHR('\x1f')),
+                   KEY_CHR('{')),
+    [KEY_8_ASTERISK] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_ctrl,
+                              KEY_ROUTER(get_shift, KEY_CHR('8'), KEY_CHR('(')),
+                              KEY_CHR('\x7F')),
+                   KEY_CHR('[')),
+    [KEY_9_OPARENTHESIS] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_shift, KEY_CHR('9'), KEY_CHR(')')),
+                   KEY_CHR(']')),
+    [KEY_0_CPARENTHESIS] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_shift, KEY_CHR('0'), KEY_CHR('=')),
+                   KEY_CHR('}')),
+    [KEY_ENTER] = KEY_ROUTER(get_new_line_mode, KEY_CHR('\x0D'), KEY_STR("\x0D\x0D")),
+    [KEY_ESCAPE] = KEY_CHR('\x1B'),
+    [KEY_BACKSPACE] =
+        KEY_ROUTER(get_ctrl,
+                   KEY_ROUTER(get_backspace_mode, KEY_CHR('\x7F'), KEY_CHR('\x08')),
+                   KEY_CHR('\x08')),
+    [KEY_TAB] = KEY_CHR('\x09'),
+    [KEY_SPACEBAR] = KEY_ROUTER(get_ctrl, KEY_CHR(' '), KEY_CHR('\0')),
+    [KEY_MINUS_UNDERSCORE] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_shift, KEY_CHR('+'), KEY_CHR('?')),
+                   KEY_CHR('\\')),
+    [KEY_EQUAL_PLUS] = // TODO! Should be a compose key
+        KEY_ROUTER(get_shift, KEY_STR("´"), KEY_CHR('`')),
+/*VERIFIED UNTIL*/
+    [KEY_OBRACKET_AND_OBRACE] =
+        KEY_ROUTER(get_shift, KEY_STR("å"), KEY_STR("Å")),
+    [KEY_CBRACKET_AND_CBRACE] = // TODO! Should be a compose key
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_shift, KEY_STR("¨"), KEY_CHR('^')),
+                   KEY_CHR('~')),
+    [KEY_BACKSLASH_VERTICAL_BAR] =
+        KEY_ROUTER(get_shift, KEY_CHR('\''), KEY_CHR('*')),
+    [KEY_SEMICOLON_COLON] =
+        KEY_ROUTER(get_shift, KEY_STR("ö"), KEY_STR("Ö")),
+    [KEY_SINGLE_AND_DOUBLE_QUOTE] =
+        KEY_ROUTER(get_shift, KEY_STR("ä"), KEY_STR("Ä")),
+    [KEY_GRAVE_ACCENT_AND_TILDE] =
+        KEY_ROUTER(get_shift, KEY_STR("§"), KEY_STR("½")),
+    [KEY_COMMA_AND_LESS] =
+        KEY_ROUTER(get_shift, KEY_CHR(','), KEY_CHR(';')),
+    [KEY_DOT_GREATER] =
+        KEY_ROUTER(get_shift, KEY_CHR('.'), KEY_CHR(':')),
+    [KEY_SLASH_QUESTION] =
+        KEY_ROUTER(get_shift, KEY_CHR('-'), KEY_CHR('_')),
+    [KEY_CAPS_LOCK] = KEY_HANDLER(handle_caps_lock),
+    [KEY_F1] = KEY_SS3_MOD_CSI("P"),
+    [KEY_F2] = KEY_SS3_MOD_CSI("Q"),
+    [KEY_F3] = KEY_SS3_MOD_CSI("R"),
+    [KEY_F4] = KEY_SS3_MOD_CSI("S"),
+    [KEY_F5] = KEY_CSI("15~"),
+    [KEY_F6] = KEY_CSI("17~"),
+    [KEY_F7] = KEY_CSI("18~"),
+    [KEY_F8] = KEY_CSI("19~"),
+    [KEY_F9] = KEY_CSI("20~"),
+    [KEY_F10] = KEY_CSI("21~"),
+    [KEY_F11] = KEY_CSI("23~"),
+    [KEY_F12] = KEY_CSI("24~"),
+    [KEY_SCROLL_LOCK] = KEY_HANDLER(handle_scroll_lock),
+    [KEY_INSERT] = KEY_CSI("2~"),
+    [KEY_HOME] =
+        KEY_ROUTER(get_keyboard_compatibility,
+                   KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1_1("H"), KEY_SS3("H")),
+                   KEY_CSI("1~")),
+    [KEY_PAGEUP] = KEY_CSI("5~"),
+    [KEY_DELETE] = KEY_ROUTER(get_ctrl_alt,
+                              KEY_CSI("3~"),
+                              KEY_HANDLER(handle_ctrl_alt_delete)),
+    [KEY_END1] =
+        KEY_ROUTER(get_keyboard_compatibility,
+                   KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("F"), KEY_SS3("F")),
+                   KEY_CSI("4~")),
+    [KEY_PAGEDOWN] = KEY_CSI("6~"),
+    [KEY_RIGHTARROW] =
+        KEY_ROUTER(get_ansi_mode,
+                   KEY_ESC("C"),
+                   KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("C"), KEY_SS3("C"))),
+    [KEY_LEFTARROW] =
+        KEY_ROUTER(get_ansi_mode,
+                   KEY_ESC("D"),
+                   KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("D"), KEY_SS3("D"))),
+    [KEY_DOWNARROW] =
+        KEY_ROUTER(get_ansi_mode,
+                   KEY_ESC("B"),
+                   KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("B"), KEY_SS3("B"))),
+    [KEY_UPARROW] =
+        KEY_ROUTER(get_ansi_mode,
+                   KEY_ESC("A"),
+                   KEY_ROUTER(get_cursor_key_mode, KEY_CSI_MOD_1("A"), KEY_SS3("A"))),
+    [KEY_KEYPAD_NUM_LOCK_AND_CLEAR] = KEY_HANDLER(handle_num_lock),
+    [KEY_KEYPAD_SLASH] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode, KEY_ESC("?o"), KEY_SS3("o")),
+                   KEY_CHR('/')),
+    [KEY_KEYPAD_ASTERIKS] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode, KEY_ESC("?j"), KEY_SS3("j")),
+                   KEY_CHR('*')),
+    [KEY_KEYPAD_MINUS] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode, KEY_ESC("?m"), KEY_SS3("m")),
+                   KEY_CHR('-')),
+    [KEY_KEYPAD_PLUS] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode, KEY_ESC("?k"), KEY_SS3("k")),
+                   KEY_CHR('+')),
+    [KEY_KEYPAD_ENTER] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode, KEY_ESC("?M"), KEY_SS3("M")),
+                   KEY_ROUTER(get_new_line_mode, KEY_CHR('\x0d'), KEY_STR("\x0d\x0a"))),
+    [KEY_KEYPAD_1_END] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?q"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("F"), KEY_SS3("q"))),
+                   KEY_CHR('1')),
+    [KEY_KEYPAD_2_DOWN_ARROW] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?r"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("B"), KEY_SS3("r"))),
+                   KEY_CHR('2')),
+    [KEY_KEYPAD_3_PAGEDN] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?s"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_CSI("6~"), KEY_SS3("s"))),
+                   KEY_CHR('3')),
+    [KEY_KEYPAD_4_LEFT_ARROW] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?t"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("D"), KEY_SS3("t"))),
+                   KEY_CHR('4')),
+    [KEY_KEYPAD_5] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?u"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("E"), KEY_SS3("u"))),
+                   KEY_CHR('5')),
+    [KEY_KEYPAD_6_RIGHT_ARROW] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?v"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("C"), KEY_SS3("v"))),
+                   KEY_CHR('6')),
+    [KEY_KEYPAD_7_HOME] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?w"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("H"), KEY_SS3("w"))),
+                   KEY_CHR('7')),
+    [KEY_KEYPAD_8_UP_ARROW] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?x"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_SS3("A"), KEY_SS3("x"))),
+                   KEY_CHR('8')),
+    [KEY_KEYPAD_9_PAGEUP] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?y"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_CSI("5~"), KEY_SS3("y"))),
+                   KEY_CHR('9')),
+    [KEY_KEYPAD_0_INSERT] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ansi_mode,
+                              KEY_ESC("?p"),
+                              KEY_ROUTER(get_keyboard_compatibility,
+                                         KEY_CSI("2~"), KEY_SS3("p"))),
+                   KEY_CHR('0')),
+    [KEY_KEYPAD_DECIMAL_SEPARATOR_DELETE] =
+        KEY_ROUTER(get_num,
+                   KEY_ROUTER(get_ctrl_alt,
+                              KEY_ROUTER(get_ansi_mode,
+                                         KEY_ESC("?n"),
+                                         KEY_ROUTER(get_keyboard_compatibility,
+                                                    KEY_CSI("3~"), KEY_SS3("n"))),
+                              KEY_HANDLER(handle_ctrl_alt_delete)),
+                   KEY_CHR(',')),
+    [KEY_NONUS_BACK_SLASH_VERTICAL_BAR] =
+        KEY_ROUTER(get_alt_gr,
+                   KEY_ROUTER(get_case, KEY_CHR('<'), KEY_CHR('>')),
+                   KEY_CHR('|')),
+
+};
+
 static void transmit_escape_key(struct terminal *terminal, character_t control,
                                 const char *string, size_t mod_prepend_1s,
                                 bool mod_force_csi) {
@@ -1121,6 +1464,10 @@ void terminal_keyboard_init(struct terminal *terminal,
     break;
   case KEYBOARD_LAYOUT_DE:
     terminal->keys_entries = de_entries;
+    terminal->alt_gr = true;
+    break;
+  case KEYBOARD_LAYOUT_SE:
+    terminal->keys_entries = se_entries;
     terminal->alt_gr = true;
     break;
   case KEYBOARD_LAYOUT_US:
