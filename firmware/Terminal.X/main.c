d0dc89c8b813653d6d15e83a4b577761f5945888
Peter Hizalev <peter.hizalev@gmail.com>
2022-06-07 09:47:59 -0700

    Remove volatile const workaround

--- firmware/Terminal.X/main.c
+++ firmware/Terminal.X/main.c
@@ -55,7 +55,6 @@
 #include "main.h"
 #include "terminal.h"
 #include "terminal_config_ui.h"
-#include "terminal_config_nvm.h"
 #include "screen.h"
 #include "vga.h"
 #include "normal.h"
@@ -117,6 +116,39 @@ int USBSerialRxBufTail = 0;
 
 volatile int LEDTimer = 0;
 volatile int GeneralTimer;
+
+__attribute__((aligned(1024), space(prog), section(".nvm")))
+volatile const struct terminal_config terminal_config = {
+    .format_rows = FORMAT_24_ROWS,
+    .monochrome_transform = MONOCHROME_TRANSFORM_LUMINANCE,
+
+    .baud_rate = BAUD_RATE_115200,
+    .stop_bits = STOP_BITS_1,
+    .parity = PARITY_NONE,
+    .serial_inverted = false,
+
+    .charset = CHARSET_UTF8,
+    .keyboard_compatibility = KEYBOARD_COMPATIBILITY_PC,
+    .keyboard_layout = KEYBOARD_LAYOUT_US,
+    .receive_c1_mode = C1_MODE_8BIT,
+    .transmit_c1_mode = C1_MODE_7BIT,
+
+    .auto_wrap_mode = true,
+    .screen_mode = false,
+
+    .send_receive_mode = true,
+
+    .new_line_mode = false,
+    .cursor_key_mode = false,
+    .auto_repeat_mode = true,
+    .ansi_mode = true,
+    .backspace_mode = false,
+    .application_keypad_mode = false,
+
+    .flow_control = true,
+
+    .start_up = START_UP_MESSAGE,
+};
 
 #define FONT_WIDTH 8
 #define FONT_HEIGHT 16
