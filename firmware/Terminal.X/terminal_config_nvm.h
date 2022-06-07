1230293fcbbf84db538dd9afdb16be3ef2cef508
Peter Hizalev <peter.hizalev@gmail.com>
2022-06-07 09:47:59 -0700

    Remove volatile const workaround

--- firmware/Terminal.X/terminal_config_nvm.h
+++
@@ -1,8 +0,0 @@
-#include "terminal.h"
-
-// This is the workaround for xc32 v3 compiler bug causing
-// "Incompatible section flags for identifier terminal_config" error.
-//
-// See: https://www.microchip.com/forums/m1173831.aspx
-
-extern volatile const struct terminal_config terminal_config;
